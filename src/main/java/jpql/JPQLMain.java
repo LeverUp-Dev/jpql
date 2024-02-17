19cac5c14745f660cf24c40d5dcb5ce4a80a7a82
LeverUp-Dev <123243889+LeverUp-Dev@users.noreply.github.com>
2024-02-17 23:08:44 +0900

    JPQL_기본_문법
    query 1, 2는 반환 타입이 명확할 때 사용.
    
    3은 반환타입이 명확하지 않을 때 사용

---
+++ src/main/java/jpql/JPQLMain.java
@@ -0,0 +1,40 @@
+package jpql;
+
+import jakarta.persistence.*;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+
+import java.util.List;
+
+public class JPQLMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
+            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
+            Query query3 = em.createQuery("select m.username from Member m", String.class);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
